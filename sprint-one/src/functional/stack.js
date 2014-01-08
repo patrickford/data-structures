11737fa6f160006ba7c056736a3dbc5e16b2d217
Patrick Ford <patrickford@msn.com>
2014-01-08 12:26:51 -0800

    Increments and decrements size. Implements .pop method.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,21 +1,27 @@
 var makeStack = function(){
-    var instance = {};
+  var instance = {};
 
   // Use an object with numeric keys to store values
-    var storage = {};
-  /* START PROMPT
-    var size; // Hint: set an initial value here
-  -END PROMPT */
+  var storage = {};
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   instance.push = function(value){
+    size++;
   };
 
   instance.pop = function(){
+    if (size) {
+      size--;
+      var result = storage[size];
+      delete storage[size];
+    }
+    return result;
   };
 
   instance.size = function(){
+    return size;
   };
   
-    return instance;
+  return instance;
   };
