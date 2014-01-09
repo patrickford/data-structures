218dc8ec33ff2b35a24c3494d3b5c190f6418142
Patrick Ford <patrickford@msn.com>
2014-01-09 14:24:44 -0800

    Linked List implemented.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (this.head === null) {
+      this.head = makeNode(value);
+      this.tail = this.head
+    } 
+    else {
+      this.tail.next = makeNode(value);
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    toBeRemoved = this.head;
+    this.head = this.head.next
+    delete toBeRemoved;
   };
 
   list.contains = function(target, node){
+    var currentNode = this.head;
+    var result = false;
+    while(currentNode){
+      if(currentNode.value === target){
+        result = true;
+      }
+      currentNode = currentNode.next;
+    }
+    return result;
   };
 
   return list;
@@ -22,3 +42,4 @@ var makeNode = function(value){
 
   return node;
 };
+ 
