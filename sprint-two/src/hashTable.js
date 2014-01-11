b087a826f800ee2649e7f9464425f89cf919af7d
Patrick Ford <patrickford@msn.com>
2014-01-11 14:25:26 -0800

    Initial setup of hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,20 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i,v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(val, index, coll){
+    if(index === i){
+      coll[index] = null;
+    }
+  });
+//this._storage.set(i, null);
 };
-
